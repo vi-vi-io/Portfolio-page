a801bcf192b67d17553625b5228edb36d40aeb43
R. Scott Vivilecchia <rscott@vi-vi.io>
2021-09-30 21:24:59 -0400

    Ajax form submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,33 @@ $window.on('resize', resizeMovie); // optimize movie crop on window dimension ch
 
 		$this.scrollex(options);
 	});
+
+	// Send AJAX Contact form data to Netlify
+	function sendFormData() {
+
+		function sendData() {
+			var XHR = new XMLHttpRequest()
+			var FD = new FormData(form)
+			XHR.addEventListener('load', function(event) {
+				form.classList.add('inactive')
+				var success = document.querySelectorAll('.post-sent')[0]
+				success.classList.add('active')
+			})
+			XHR.addEventListener('error', function(event) {
+				form.classList.add('inactive')
+				var error = document.querySelectorAll('.post-error')[0]
+				error.classList.add('active')
+			})
+			XHR.open('POST', '#')
+			XHR.send(FD)
+		}
+	  
+		var form = document.querySelectorAll('.contact')[0]
+		form.addEventListener('submit', function (e) {
+			e.preventDefault()
+			sendData()
+		})
+	  
+	}
+	  
+	export default sendFormData
